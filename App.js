3f959aaa37421930e4b945dc0be1a10209dfbe4c
Pratik Pandya <46597115+PratikPandyaOfficial@users.noreply.github.com>
2021-04-04 01:29:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+ import React from 'react';
+ import { Text, View } from 'react-native';
+
+ const HelloWord = () => {
+	 return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+		<Text>
+			HelloWord
+		</Text>
+	</View>
+	 );
+ }
+ export default HelloWord;
